08132acf4177e63b3a043ec3d06251d907c8edba
“Gjorgi <georgiedelev@gmail.com>
2023-05-09 12:29:23 +0200

    overwriting statuc rules

--- users/views.py
+++ users/views.py
@@ -21,3 +21,7 @@ def user_login(request):
 
     form = LoginForm()
     return render(request, 'users/login.html', {'form': form})
+
+
+def index(request):
+    return render(request, 'users/index.html')
