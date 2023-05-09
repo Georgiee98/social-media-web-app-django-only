730ba020ffaab656c240d21255bff52803616fd4
“Gjorgi <georgiedelev@gmail.com>
2023-05-09 22:57:22 +0200

    fixing password do not match

--- users/forms.py
+++ users/forms.py
@@ -10,11 +10,15 @@ class UserRegistrationForm(forms.ModelForm):
 
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name']
 
-    def check_password(self):
-        if self.cleaned_data['password'] != self.cleaned_data['password2']:
-            raise forms.ValidationError('Password do not match')
-        return self.cleaned_data['password2']
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        if password and password2 and password != password2:
+            raise forms.ValidationError('Passwords do not match.')
+        return cleaned_data
