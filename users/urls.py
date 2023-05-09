6dc8d15b755ceb0ff057b1da4b768d5fc9821a16
“Gjorgi <georgiedelev@gmail.com>
2023-05-09 10:54:11 +0200

    logout template

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,5 @@ from . import views
 from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('login/', views.user_login, name='login'),
-    path('logout/', auth_view.LogoutView.as_view(), name='logout'),
-
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
