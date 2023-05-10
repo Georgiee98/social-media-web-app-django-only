68670bc8e182aa72d5b3d5fd3e173eccd495a0da
“Gjorgi <georgiedelev@gmail.com>
2023-05-10 22:38:08 +0200

    creating home page

--- project/urls.py
+++ project/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls'), name='login'),
     path('posts/', include('posts.urls'), name='posts'),
-] + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
