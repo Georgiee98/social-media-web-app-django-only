f60a9039b736d0362e1b3ce71fdf1784f28643dd
“Gjorgi <georgiedelev@gmail.com>
2023-05-10 20:24:39 +0200

    change in order of date

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    profile_pic = models.ImageField(upload_to='users/%d/%m/%Y', blank=True)
 
 
     def __str__(self):
