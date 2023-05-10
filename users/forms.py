66f2da9e02c8c61fbdfcddb03edbc47772f61a60
“Gjorgi <georgiedelev@gmail.com>
2023-05-10 20:27:46 +0200

    User Edit Form

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
+
+
+class UserEditForm(form.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
 
 class LoginForm(forms.Form):
     username = forms.CharField()
