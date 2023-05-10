c87d19981cded905fbd6890453b5777651fc32e8
“Gjorgi <georgiedelev@gmail.com>
2023-05-10 22:20:11 +0200

    submitting post

--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def post_create(request):
     if request.method == "POST":
-        form = PostCreateForm(data=request.POST)
+        form = PostCreateForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             new_item = form.save(commit=False)
             new_item.user = request.user
