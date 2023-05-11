07e7451e9b927210be8dd738c56a43f17581abfe
“Gjorgi <georgiedelev@gmail.com>
2023-05-12 00:44:27 +0200

    Like Functionality

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('create/', views.post_create, name='create'),
     path('feed/', views.feed, name='feed'),
+    path('like/', views.like, name='like'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from django.shortcuts import get_object_or_404
+
 
 @login_required
 def post_create(request):
@@ -18,4 +20,24 @@ def post_create(request):
 
 def feed(request):
     posts = Post.objects.all()
-    return render(request, 'posts/feed.html', {'posts': posts})
+    logged_user = request.user
+    return render(request, 'posts/feed.html', {'posts': posts, 'logged_user': logged_user})
+
+from django.http import JsonResponse
+
+def like(request):
+  post_id = request.POST.get('post_id')
+  post = get_object_or_404(Post, id=post_id)
+  liked = False
+  if post.liked_by.filter(id=request.user.id).exists():
+    post.liked_by.remove(request.user)
+  else:
+    post.liked_by.add(request.user)
+    liked = True
+  post.save()
+  response_data = {
+    'liked': liked,
+    'like_count': post.liked_by.count(),
+    'already_liked': post.liked_by.filter(id=request.user.id).exists(),
+  }
+  return JsonResponse(response_data)
